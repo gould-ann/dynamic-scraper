579f6be1e7b9a87a61cff830b0c2d8875b5abbfa
Ann Gould <gould.ann8@gmail.com>
2020-01-26 22:09:16 -0600

    added beginning zipcode functionality

--- zipcodes.py
+++ zipcodes.py
@@ -1,4 +1,20 @@
 import requests
 import json
 
-def get_nearby_zipcodes(zipcode: str):
+'''
+FOR GETTING API KEY FROM ENVIRONMENT VARIABLES
+USES API FROM https://www.zipcodeapi.com/API
+'''
+import os
+from dotenv import load_dotenv
+load_dotenv()
+zipcode_auth_key = os.getenv("ZIPCODE_AUTH_KEY")
+
+
+def get_nearby_zipcodes(zipcode: str, distance: str) -> json:
+    r = requests.get(" https://www.zipcodeapi.com/rest/" + zipcode_auth_key +  "/radius.json/" + zipcode + "/" + distance
+                 + " /mi ")
+    return r.json()
+    # print(r.json())
+
+print(get_nearby_zipcodes("50014", "10"))
